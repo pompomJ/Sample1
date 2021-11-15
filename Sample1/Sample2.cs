8b1981dad80a15ddb3dc281e4b1ed30cc2a4601e
ZamaVerce2 <Verce@LAPTOP-8JD0V3Q5>
2021-11-15 18:28:54 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample1/Sample2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sample
+{
+    class Sample2
+    { 
+    
+        static string program_ver = "sample #2";
+
+        static void Main()
+        {
+            Console.WriteLine(program_ver);
+            Console.Write("お名前は？>> ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"ようこそ、{name}さん！");
+        }
+    }
+}
